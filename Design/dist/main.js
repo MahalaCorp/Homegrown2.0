bcde6b6c7c39a9c706e7c545cd3c8c9891540978
Leomhango <leoflightmhango@gmail.com>
2022-08-12 03:08:34 +0530

    did some few changes on the contact page

--- Design/dist/main.js
+++ Design/dist/main.js
@@ -1,23 +1,24 @@
-// loader elements
-const loader = document.getElementById("loader");
-const main = document.getElementById("main");
+// // loader elements
+// const loader = document.getElementById("loader");
+// const main = document.getElementById("main");
 
-function download() {
-    if (document.all) {
-        loader.classList.add("hidden");
-        loader.classList.remove("flex");
+// function download() {
+//     if (document.all) {
+//         loader.classList.add("hidden");
+//         loader.classList.remove("flex");
 
-        main.classList.add("flex");
-        main.classList.remove("hidden");
-    } else if (document.getElementById) {
-        node = document.getElementById("loader").classList = "hidden";
-        node = document.getElementById("main").classList = "flex";
-    }
-}
+//         main.classList.add("flex");
+//         main.classList.remove("hidden");
+//     } else if (document.getElementById) {
+//         node = document.getElementById("loader").classList = "hidden";
+//         node = document.getElementById("main").classList = "flex";
+//     }
+// }
 
 //navigation elements
 const bars = document.getElementById("bars");
 const nav = document.getElementById("nav");
+const main = document.getElementsByTagName('main');
 
 bars.addEventListener('click', () => {
     if (nav.classList.contains("hidden")) {
@@ -29,19 +30,9 @@ bars.addEventListener('click', () => {
     }
 })
 
-// // talent visiblity
-// const talent = document.getElementById("talent");
-// const info = document.getElementById("info");
-
-// talent.addEventListener('mouseover', () => {
-//     if (info.classList.contains("hidden")) {
-//         info.classList.remove('hidden');
-//         info.classList.add('flex');
-//     }
-// })
-// talent.addEventListener('mouseout', () => {
-//     if (info.classList.contains("flex")) {
-//         info.classList.remove('flex');
-//         info.classList.add('hidden');
-//     }
-// })
+main.addEventListener('click', () => {
+    if (nav.classList.contains('flex')) {
+        nav.classList.remove('flex');
+        nav.classList.add('hidden')
+    }
+})
