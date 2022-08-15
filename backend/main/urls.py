754d3ffaa735dbe767ce796d8dd8a39985fe5897
Leomhango <leoflightmhango@gmail.com>
2022-08-16 01:09:53 +0530

    finished configuring urls

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -5,5 +5,11 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('blog', views.blog, name="blog"),
+    path('contact', views.contact, name="contact"),
+    path('join', views.join, name="join"),
+    path('talent', views.talent, name="talent"),
+
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -5,3 +5,31 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def blog(request):
+    return render(request, 'main/blog.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+
+def join(request):
+    return render(request, 'main/join.html')
+
+
+def talent(request):
+    return render(request, 'main/talent.html')
+
+
+def blogPost(request):
+    return render(request, 'main/blogPost.html')
+
+
+def model(request):
+    return render(request, 'main/model.html')
