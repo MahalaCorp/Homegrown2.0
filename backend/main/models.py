c46d3b099871df2268907eb4dbf5161e2d3e138f
Leomhango <leoflightmhango@gmail.com>
2022-08-16 07:45:50 +0530

    trying to add blog model

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,24 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    coverImage = models.ImageField(upload_to='blog')
+    author = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, null=True)
+
+    description = models.TextField()
+    firstParagraph = models.TextField()
+    secondParagraph = models.TextField(null=True)
+    thirdParagraph = models.TextField(null=True)
+    conclusion = models.TextField()
+
+    image1 = models.ImageField(upload_to='blog')
+    image2 = models.ImageField(upload_to='blog')
+    image3 = models.ImageField(upload_to='blog')
+
+    def __str__(self):
+        return self.title
